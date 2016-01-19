d9e75d5d204bab9b50e64dd0ecbc31ccaaddb284
Michael Sumner <Michael.Sumner@aad.gov.au>
2016-01-19 17:21:03 +1100

    minor

--- R/splat.r
+++ R/splat.r
@@ -12,6 +12,18 @@ splatchain <- function(grid, chain) {
   do.call(bind_rows, l)
 }
 
+cellnumber.pimg <- function (x) 
+{
+  xbnd <- x$xbound
+  ybnd <- x$ybound
+  offs <- x$p[[1]]$offset
+  img <- x$p[[1]]$image
+  tl <- ((ybnd[3L] - (offs[2L] + ncol(img))) + 1L) * xbnd[3L] + 
+    offs[1L]
+  sort(rep(seq(tl, by = xbnd[3], length = ncol(img)), each = nrow(img)) + 
+         rep(seq_len(nrow(img)) - 1, ncol(img)))
+}
+
 
 #' Title
 #'
@@ -20,19 +32,19 @@ splatchain <- function(grid, chain) {
 #'
 #' @return list of data_frames
 #' @export
-#' @importFrom dplyr data_frame row_number
+#' @importFrom dplyr data_frame row_number %>%  filter_
 splat <- function(x, proj = NULL) {
-  allbins <- allcells <- vector("list", length(x))
+  allbins <- allcells <- vector("list", length(x$p))
 
 
   for (i in seq_along(allcells)) {
-    allcells[[i]] <- SGAT:::cn.pimg(x[[i]])
+    allcells[[i]] <- cellnumber.pimg(x[[i]])
     allbins[[i]] <- as.vector(x[[i]]$p[[1]]$image)
   }
   index <- lapply(seq_along(allcells), function(xa) rep(xa, length(allcells[[xa]])))
-  pix <- data_frame(cell = unlist(allcells), index = unlist(index), bin = unlist(allbins))
-  time <- data_frame(time = SGAT:::as.POSIXct.Pimage(x), index = seq(length(x)))
+  pix <- dplyr::data_frame(cell = as.integer(unlist(allcells)), index = as.integer(unlist(index)), bin = as.integer(unlist(allbins)))
+  time <- dplyr::data_frame(time = SGAT:::as.POSIXct.Pimage(x), index = as.integer(seq(length(x))))
   ## filter out zeroes and the performance via dplyr is pretty close
-  list(pixel = pix %>% filter(bin > 0), time = time)
+  list(pixel = pix %>% dplyr::filter_("bin > 0"), time = time)
 }
 
